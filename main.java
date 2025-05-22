0fbbcd41a05d1c1d01dd7a3a0b4b93cfd293d738
Kamil Trzykowski <s33609@pjwstk.edu.pl>
2025-05-22 20:49:23 +0200

    1 and 4 done

--- main.java
+++ main.java
@@ -1,8 +1,14 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+//        cars();
+        zad4();
+    }
+    private static void cars(){
         Car auto1 = new Car("Skyline", 1997);
         Car auto2 = new Car("Corolla", 2003);
         Car auto3 = new Car("E46", 1998);
@@ -27,6 +33,39 @@ public class main {
         Collections.sort(cars);
         for (Car car : cars) {
             System.out.println(car);
+        }
+    }
+    private static void zad4(){
+        int[] array = new int[10];
+        fillArray(array);
+        System.out.printf("Array: ");
+        for (int i = 0; i < array.length; i++) {
+            if(i == array.length-1){
+                System.out.printf(array[i] + ".");
+            }
+            else System.out.printf(array[i] + ", ");
+        }
+    }
+
+    private static int readNumber() throws NegativeNumberException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int x = sc.nextInt();
+        if(x < 0) {
+            throw new NegativeNumberException();
+        }
+        return x;
+    }
+
+    private static void fillArray(int []array){
+        for(int i = 0; i < array.length; i++){
+            try{
+                int x = readNumber();
+                array[i] = x;
+            } catch (NegativeNumberException e) {
+                array[i] = 0;
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
